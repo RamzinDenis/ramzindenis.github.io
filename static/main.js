6255764451c204f1bf58a21f2d6eeb0ce1c52cd5
RamzinDenis <61662116+RamzinDenis@users.noreply.github.com>
2020-05-19 15:47:12 +0300

    Add files via upload

---
+++ static/main.js
@@ -0,0 +1,8 @@
+
+	const nav = document.querySelector(".menu");
+	const dropdown = document.querySelector(".menu__dropdown");
+     dropdown.onclick = function() {
+  nav.classList.toggle('is-open');
+};
+
+
